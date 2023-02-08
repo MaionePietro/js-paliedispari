167dce1a6c7f048d98c66ce25a1bd1fd0ee9273f
MaionePietro <pietromaionespam@gmail.com>
2023-02-08 16:13:23 +0100

    FEAT contollore 1 a 5

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
-
-
+/* Palidroma */
+//const nome = parseInt(prompt("inserisci nome"));
 
 /* Pari e Dispari */
 
 //inserire numero
-const input = parseInt(prompt("inserisci 1 per pari 2 per dispari"));
-console.log('hai inserito: ' + input);
+let input = parseInt(prompt("inserisci numero da 1 a 5"));
+while (input <= 5) {
+    input = parseInt(prompt("inserisci numero da 1 a 5"));
+    console.log('ok')
+}
+
+console.log('numero casuale: ' + random());
+
+//sommare input e random
+somma = input + random();
+console.log('la somma: ' + somma);
+
+pariDispari(somma);
+
 
 //generare numero random con funzione
 function random(numRandom) {
     numRandom = Math.floor(Math.random() * 5);
     return numRandom;
 }
-console.log('numero casuale: ' + random());
-
-//sommare input e random
-somma = input + random();
-console.log('la somma: ' + somma);
 
 //funzione pari dispari
 function pariDispari(num) {
@@ -26,4 +33,3 @@ function pariDispari(num) {
         console.log('dispari');
     }
 }
-pariDispari(somma);
