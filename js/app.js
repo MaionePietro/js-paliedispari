aa15732a612e7d2bc69a6f8709ee618655286008
MaionePietro <pietromaionespam@gmail.com>
2023-02-08 15:50:32 +0100

    formattazione documento

--- js/app.js
+++ js/app.js
@@ -1,31 +1,29 @@
 
 
 
-// Pari e Dispari
+/* Pari e Dispari */
 
 //inserire numero
 const input = parseInt(prompt("inserisci 1 per pari 2 per dispari"));
-console.log('hai inserito: '+input);
+console.log('hai inserito: ' + input);
 
 //generare numero random con funzione
-
-function random(numRandom){
-    numRandom = Math.floor(Math.random()*5);
+function random(numRandom) {
+    numRandom = Math.floor(Math.random() * 5);
     return numRandom;
 }
-console.log('numero casuale: '+ random());
+console.log('numero casuale: ' + random());
 
 //sommare input e random
-somma = input + random;
-console.log('la somma: '+somma);
-//funzione pari dispari
+somma = input + random();
+console.log('la somma: ' + somma);
 
-function pariDispari(num){
-    if(num%2==0){
+//funzione pari dispari
+function pariDispari(num) {
+    if (num % 2 == 0) {
         console.log('pari');
-    }else{
+    } else {
         console.log('dispari');
     }
 }
-
 pariDispari(somma);
